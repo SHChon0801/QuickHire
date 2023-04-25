4f7757772af02a7fa147b05964d4d3a80608745b
Adrian Joshua <adrianjoshua738@gmail.com>
2023-04-25 20:43:38 +0800

    Can store accounts in realtime db
    Accounts can be registered as Employee or organization

---
+++ app/src/main/java/my/edu/tarc/quickhire/User.kt
@@ -0,0 +1,3 @@
+package my.edu.tarc.quickhire
+
+data class User(val email: String? = null, val password: String? = null, val role: String? =null)
--- app/src/main/java/my/edu/tarc/quickhire/ui/register/RegisterActivity.kt
+++ app/src/main/java/my/edu/tarc/quickhire/ui/register/RegisterActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.view.isNotEmpty
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import my.edu.tarc.quickhire.R
+import my.edu.tarc.quickhire.User
 import my.edu.tarc.quickhire.databinding.ActivityRegisterBinding
 import my.edu.tarc.quickhire.ui.login.LoginActivity
 
@@ -13,24 +19,67 @@ class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var databaseReferenceOrg: DatabaseReference
+    private lateinit var databaseReferenceEmp: DatabaseReference
+    private lateinit var database: FirebaseDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReferenceOrg = database.getReference("Employees")
+        databaseReferenceEmp = database.getReference("Organizations")
+
 
 
         binding.btnRegister.setOnClickListener {
             val email = binding.editTextEmail.text.toString()
             val password = binding.editTextPassword.text.toString()
             val confirmPassword = binding.editTextConfirmPassword.text.toString()
+            val roleRadioGroup = binding.rgGroup
+            val userRole = roleRadioGroup.checkedRadioButtonId
+            var role = ""
 
-            if(email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+            when(userRole) {
+                R.id.rbEmp ->{
+                    role = "Employee"
+                }
+                R.id.rbOrg ->{
+                    role = "Organization"
+                }
+            }
+
+
+
+            if(email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty() && roleRadioGroup.checkedRadioButtonId != -1){
                 if(password == confirmPassword){
 
                     firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
                         if(it.isSuccessful){
+
+                            val newUser = User(email,password,role)
+
+                            when(userRole) {
+                                R.id.rbEmp ->{
+                                    databaseReferenceEmp.child(password).setValue(newUser)
+                                }
+                                R.id.rbOrg ->{
+                                    databaseReferenceOrg.child(password).setValue(newUser)
+
+                                }
+                            }
+
+
+
+
+
+
+
+
+
+
                             val intent = Intent(this,LoginActivity::class.java)
                             startActivity(intent)
                         }
@@ -56,6 +105,7 @@ class RegisterActivity : AppCompatActivity() {
 
     }
 }
+
 
 
 
