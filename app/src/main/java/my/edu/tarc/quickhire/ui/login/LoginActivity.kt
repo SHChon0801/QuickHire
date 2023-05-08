5a1388423c02757dfcf52613708f9b89644e593e
TARUMT <shchon1122@gmail.com>
2023-05-08 15:33:12 +0800

    homepage2.1

--- app/src/main/java/my/edu/tarc/quickhire/ui/login/LoginActivity.kt
+++ app/src/main/java/my/edu/tarc/quickhire/ui/login/LoginActivity.kt
@@ -112,12 +112,6 @@ class LoginActivity : AppCompatActivity() {
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
 
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
-
-
-
-
                     }else{
                         Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT).show()
                     }
